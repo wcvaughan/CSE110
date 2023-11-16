f1e57342a1adc4087a8dbe6563f8d7921d4e6024
wcvaughan <chrisvaughanlds@gmail.com>
2023-11-16 14:27:48 -0900

    fixed

--- week06/life-expectancy-calc.py
+++ week06/life-expectancy-calc.py
@@ -1,3 +1,6 @@
+import math
+from statistics import mean
+
 min_life_expectancy = 1000
 
 max_life_expectancy = 0
@@ -23,7 +26,7 @@ with open("week06\life-expectancy.csv") as life_files:
         line = line.split(",")
         country = line[0]
         code = line[1]
-        year = line[2]
+        year = int(line[2])
         life_expectancy = float(line[3])
         if life_expectancy < min_life_expectancy:
             min_life_expectancy = life_expectancy
@@ -37,7 +40,17 @@ with open("week06\life-expectancy.csv") as life_files:
             max_life_country = country
 
             max_life_year = year
+        if year_of_interest == year:
+            if life_expectancy < min_life_expectancy:
+                min_life_expectancy_year_of_interest = life_expectancy
 
+                min_life_expectancy_year_of_interest_country = country
+            if life_expectancy > max_life_expectancy_year_of_interest:
+                max_life_expectancy_year_of_interest = life_expectancy
+
+                max_life_expectancy_year_of_interest_country = country
+            
+            
 print(f'The overall max life expectancy is: {max_life_expectancy} from {max_life_country} in {max_life_year}')
-print(f'The overall min life expectancy is: {min_life_expectancy} from {min_life_country} in {min_life_year}')        
+print(f'The overall min life expectancy is: {min_life_expectancy} from {min_life_country} in {min_life_year}')
 print(f'For the year: {year_of_interest}\nThe average life expectancy across all countries was {average_life_expectancy_year_of_interest:.2f}\nThe max life expectancy was in {max_life_expectancy_year_of_interest_country} with {max_life_expectancy_year_of_interest:.2f}\nThe min life expectancy was in {min_life_expectancy_year_of_interest_country} with {min_life_expectancy_year_of_interest:.2f}')
